4ba44b249757023e0a8db7a2973583b36b272ba4
Jonathan-Lin-162 <ylin297@my.bcit.ca>
2026-07-05 01:22:41 -0700

    fixed the typo error from User to user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const User = require("../models/User");
+const User = require("../models/user");
 const displayMessage = require("../utils/displayMessage");
 const loginSchema = require("../validation/loginSchema");
 const signupSchema = require("../validation/signupSchema");
